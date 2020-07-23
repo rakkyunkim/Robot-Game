a88f0997cbfbf87f8c2332523dbc8ba60ac43177
rakkyunkim <59427067+rakkyunkim@users.noreply.github.com>
2020-07-22 18:24:01 -0700

    Create Game.h

---
+++ Game.h
@@ -0,0 +1,17 @@
+class Arena;
+#ifndef GAME_H
+#define GAME_H
+class Game
+{
+public:
+    // Constructor/destructor
+    Game(int rows, int cols, int nRobots);
+    ~Game();
+
+    // Mutators
+    void play();
+
+private:
+    Arena* m_arena;
+};
+#endif
